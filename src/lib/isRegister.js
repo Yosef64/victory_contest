ea84b24b3fa6bff30be2d1fe6282b027d5ad5d2b
teddy <teddytilahun02@gmail.com>
2024-11-21 11:30:46 +0300

    change useEffect by loader

--- src/lib/isRegister.js
+++ src/lib/isRegister.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export const isRegister = async (id) => {
   const url = import.meta.env.VITE_TgBot;
+  const text = "you are already registered";
+
   try {
     const res = await axios.post(
       "https://victory-tutorial-api.vercel.app/check_user",
@@ -12,17 +14,15 @@ export const isRegister = async (id) => {
         withCredentials: true,
       }
     );
+    if (res.data.message == false) {
+      await axios.get(
+        `https://api.telegram.org/bot${url}/sendMessage?chat_id=${id}&text=${text},`,
 
-    // await axios.post(
-    //   `https://api.telegram.org/bot${url}/sendMessage`,
-    //   {
-    //     chat_id: id,
-    //     text: "you are already registered",
-    //   },
-    //   {
-    //     withCredentials: true,
-    //   }
-    // );
+        {
+          withCredentials: true,
+        }
+      );
+    }
 
     return res.data.message == false;
   } catch (error) {

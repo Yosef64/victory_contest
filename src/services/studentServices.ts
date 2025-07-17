7a55721e6c55032e94e4c287ad33406b5ac26ad4
Yosef64 <yosefale65@gmail.com>
2025-07-17 21:14:10 +0300

    changeing name of api routes

--- src/services/studentServices.ts
+++ src/services/studentServices.ts
@@ -1,7 +1,7 @@
 import api from "./api";
 
 export async function getUserStat(user_id: string) {
-  const res = await api.get(`/student/${user_id}/statistics`);
+  const res = await api.get(`/student/statistics/${user_id}`);
   return res.data.message;
 }
 
@@ -10,6 +10,6 @@ export async function getUserProfile(user_id: string) {
   return res.data.message;
 }
 export async function getUserBadge(user_id: string) {
-  const res = await api.get(`/student/${user_id}/badge`);
+  const res = await api.get(`/student/student/${user_id}/badge`);
   return res.data.message;
 }

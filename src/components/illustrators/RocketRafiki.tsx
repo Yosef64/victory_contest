25835248de7d9c6bff504b43fda4353fe048b3b2
Yosef64 <yosefale65@gmail.com>
2025-07-23 22:30:29 +0300

    modifing welcoming part

---
+++ src/components/illustrators/RocketRafiki.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import UpgradeSvg from "../../assets/Upgrade-pana.svg?react";
+export const RocketRafiki: React.FC<React.SVGProps<SVGSVGElement>> = (
+  props
+) => <UpgradeSvg {...props} />;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,15 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      animation: {
+        "fade-in": "fade-in 0.5s ease-in-out",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",

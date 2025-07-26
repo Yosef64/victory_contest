9f44002990cb6cd8d20716153070f79da8baab4e
Yosef64 <yosefale65@gmail.com>
2025-07-26 04:10:20 +0300

    testing skeleton

--- src/components/LeaderboardSkeleton.tsx
+++ src/components/LeaderboardSkeleton.tsx
@@ -5,7 +5,7 @@ export function LeaderboardSkeleton() {
     <div>
       <main className="mt-10 pb-4">
         {/* Podium Skeleton */}
-        <div className="flex justify-center items-center gap-4 md:gap-8 mb-12 px-4 animate-pulse dark:bg-gray-800">
+        <div className="flex justify-center items-center gap-4 md:gap-8 mb-12 px-4 animate-pulse dark:bg-gray-900">
           {/* 2nd Place */}
           <div className="flex flex-col items-center gap-2 mt-4">
             <div className="relative">
@@ -25,7 +25,7 @@ export function LeaderboardSkeleton() {
           {/* 3rd Place */}
           <div className="flex flex-col items-center dark:bg-gray-700 gap-2 mt-4">
             <div className="relative">
-              <Skeleton className="h-20 w-20 mb-1 rounded-full border-" />
+              <Skeleton className="h-20 w-20 mb-1 rounded-full" />
             </div>
             <Skeleton className="h-5 w-32 rounded-md dark:bg-gray-700" />
             <Skeleton className="h-4 w-24 rounded-md dark:bg-gray-700" />
@@ -39,7 +39,7 @@ export function LeaderboardSkeleton() {
             {Array.from({ length: 7 }).map((_, i) => (
               <div
                 key={i}
-                className="p-4 rounded-xl border bg-card dark:bg-gray-800 text-card-foreground shadow-sm flex items-center justify-between"
+                className="p-4 rounded-xl bg-card dark:bg-gray-800 text-card-foreground flex items-center justify-between"
               >
                 <div className="flex items-center space-x-3">
                   <Skeleton className="h-6 w-6 dark:bg-gray-700 rounded-full" />

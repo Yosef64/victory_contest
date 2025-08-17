b3922f9872ef157832b3f0edc892967a514bd663
Yosef64 <yosefale65@gmail.com>
2025-08-17 17:17:12 +0300

    Feat:add a payment Alert

---
+++ src/components/PaymentAlert.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { PaymentRequest } from "../types";
+import { fetchUserPaymentRequests } from "../services/paymentServices";
+import { useTelegram } from "../hooks/useTelegram";
+import { Alert, AlertTitle } from "../components/ui/alert";
+import { PopcornIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+export default function PaymentAlert() {
+  const { user: tgUser } = useTelegram();
+  const navigate = useNavigate();
+
+  const [payments, setpayments] = useState<PaymentRequest[] | null>(null);
+
+  useEffect(() => {
+    if (!tgUser?.id) return;
+    const fetchUserPayments = async () => {
+      try {
+        const payments = await fetchUserPaymentRequests(tgUser?.id.toString());
+        setpayments(payments);
+      } catch (err) {}
+    };
+    fetchUserPayments();
+  }, [tgUser]);
+
+  if (payments == null) {
+    return;
+  }
+
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+
+  const isAboutToExpire = payments.some((payment) => {
+    if (payment.status !== "Approved" || !payment.expirationDate) {
+      return false;
+    }
+    const timeDiff = new Date(payment.expirationDate).getTime() - Date.now();
+    return timeDiff <= THREE_DAYS_IN_MS && timeDiff >= 0;
+  });
+
+  if (!isAboutToExpire) {
+    return null;
+  }
+  return (
+    <Alert variant="warning">
+      <PopcornIcon />
+      <AlertTitle>
+        Your Payment is going to be expired. Please subscribe for more.{" "}
+        <span
+          onClick={() => navigate("/payment")}
+          role="button"
+          tabIndex={0}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" || e.key === " ") navigate("/payment");
+          }}
+          className="underline cursor-pointer"
+        >
+          Subscribe
+        </span>
+      </AlertTitle>
+    </Alert>
+  );
+}

49fad6a4cc29445d599941854cd995f3b3dd51c8
Yosef64 <yosefale65@gmail.com>
2025-08-27 14:28:20 +0300

    removing reply markup and check the save message

--- src/components/article/ArticleView.tsx
+++ src/components/article/ArticleView.tsx
@@ -169,26 +169,11 @@ export function ArticleView() {
         id: article.id, // 1-64 chars
         title: article.title,
         input_message_content: {
-          message_text: `<strong>${article.title}</strong>\n\n${article.excerpt}\n\nRead more`,
+          message_text: `<strong>${article.title}</strong>\n\n${article.excerpt}\n\n<a href="https://victory-contest.com/article/${article.id}">Read more</a>\n https://t.me/victory_contest_bot/victory`,
           parse_mode: "HTML",
         },
         description: article.excerpt,
         thumbnail_url: article.thumbnail || "https://picsum.photos/200/300",
-        // thumbnail_width: 100,
-        // thumbnail_height: 100,
-        // reply_markup: {
-        //   inline_keyboard: [
-        //     [
-        //       {
-        //         text: "Read Full Article",
-        //         web_app: {
-        //           url:
-        //             "https://victory-contest.vercel.app/article/" + article.id,
-        //         },
-        //       },
-        //     ],
-        //   ],
-        // },
       };
 
       try {

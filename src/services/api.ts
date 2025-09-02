2f4c595c32c00246ee75357c39b912219d1e1edd
Yosef64 <yosefale65@gmail.com>
2025-09-02 13:31:46 +0300

    changing base api base url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 //const baseURL = "https://7wwb0knl-8080.euw.devtunnels.ms";
-const baseURL = "https://7wwb0knl-8080.euw.devtunnels.ms";
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
   baseURL: baseURL + "/api",
   withCredentials: true,
